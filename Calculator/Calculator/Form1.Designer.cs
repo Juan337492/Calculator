c37d093ef679b9f06d1d432cdd411cb00cac895c
Juan Rodriguez <Juan337492@yahoo.com>
2021-02-07 16:00:44 -0500

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.addBtn = new System.Windows.Forms.RadioButton();
+            this.subtractBtn = new System.Windows.Forms.RadioButton();
+            this.multiplyBtn = new System.Windows.Forms.RadioButton();
+            this.divideBtn = new System.Windows.Forms.RadioButton();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.colorPicker = new System.Windows.Forms.ColorDialog();
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.btnColorPicker = new System.Windows.Forms.Button();
+            this.helpMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuItems = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpMenu.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addBtn
+            // 
+            this.addBtn.AutoSize = true;
+            this.addBtn.Location = new System.Drawing.Point(267, 125);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(31, 17);
+            this.addBtn.TabIndex = 0;
+            this.addBtn.TabStop = true;
+            this.addBtn.Text = "+";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // subtractBtn
+            // 
+            this.subtractBtn.AutoSize = true;
+            this.subtractBtn.Location = new System.Drawing.Point(267, 156);
+            this.subtractBtn.Name = "subtractBtn";
+            this.subtractBtn.Size = new System.Drawing.Size(28, 17);
+            this.subtractBtn.TabIndex = 1;
+            this.subtractBtn.TabStop = true;
+            this.subtractBtn.Text = "-";
+            this.subtractBtn.UseVisualStyleBackColor = true;
+            // 
+            // multiplyBtn
+            // 
+            this.multiplyBtn.AutoSize = true;
+            this.multiplyBtn.Location = new System.Drawing.Point(266, 189);
+            this.multiplyBtn.Name = "multiplyBtn";
+            this.multiplyBtn.Size = new System.Drawing.Size(29, 17);
+            this.multiplyBtn.TabIndex = 2;
+            this.multiplyBtn.TabStop = true;
+            this.multiplyBtn.Text = "*";
+            this.multiplyBtn.UseVisualStyleBackColor = true;
+            // 
+            // divideBtn
+            // 
+            this.divideBtn.AutoSize = true;
+            this.divideBtn.Location = new System.Drawing.Point(265, 222);
+            this.divideBtn.Name = "divideBtn";
+            this.divideBtn.Size = new System.Drawing.Size(30, 17);
+            this.divideBtn.TabIndex = 3;
+            this.divideBtn.TabStop = true;
+            this.divideBtn.Text = "/";
+            this.divideBtn.UseVisualStyleBackColor = true;
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(47, 122);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(184, 20);
+            this.input1.TabIndex = 4;
+            // 
+            // input2
+            // 
+            this.input2.Location = new System.Drawing.Point(337, 122);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(180, 20);
+            this.input2.TabIndex = 5;
+            // 
+            // answerBox
+            // 
+            this.answerBox.Location = new System.Drawing.Point(565, 122);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.Size = new System.Drawing.Size(168, 20);
+            this.answerBox.TabIndex = 6;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.calculateBtn.Location = new System.Drawing.Point(102, 330);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(149, 43);
+            this.calculateBtn.TabIndex = 7;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clearBtn.Location = new System.Drawing.Point(321, 330);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(129, 43);
+            this.clearBtn.TabIndex = 8;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exitBtn.Location = new System.Drawing.Point(518, 330);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(117, 43);
+            this.exitBtn.TabIndex = 9;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // colorPicker
+            // 
+            this.colorPicker.Color = System.Drawing.Color.DarkGray;
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.colorBtn.Location = new System.Drawing.Point(565, 27);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(140, 23);
+            this.colorBtn.TabIndex = 10;
+            this.colorBtn.Text = "Background color picker";
+            this.colorBtn.UseVisualStyleBackColor = true;
+            this.colorBtn.Click += new System.EventHandler(this.colorBtn_Click);
+            // 
+            // btnColorPicker
+            // 
+            this.btnColorPicker.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnColorPicker.Location = new System.Drawing.Point(565, 56);
+            this.btnColorPicker.Name = "btnColorPicker";
+            this.btnColorPicker.Size = new System.Drawing.Size(140, 23);
+            this.btnColorPicker.TabIndex = 11;
+            this.btnColorPicker.Text = "Button color picker";
+            this.btnColorPicker.UseVisualStyleBackColor = true;
+            this.btnColorPicker.Click += new System.EventHandler(this.btnColorPicker_Click);
+            // 
+            // helpMenu
+            // 
+            this.helpMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.helpMenu.Name = "helpMenu";
+            this.helpMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.helpMenu.Size = new System.Drawing.Size(108, 26);
+            this.helpMenu.Text = "Menu";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(107, 22);
+            this.toolStripMenuItem1.Text = "About";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItems});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuItems
+            // 
+            this.menuItems.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.backgroundColorToolStripMenuItem,
+            this.buttonColorToolStripMenuItem,
+            this.calculateToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuItems.Name = "menuItems";
+            this.menuItems.Size = new System.Drawing.Size(37, 20);
+            this.menuItems.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // calculateToolStripMenuItem
+            // 
+            this.calculateToolStripMenuItem.Name = "calculateToolStripMenuItem";
+            this.calculateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.calculateToolStripMenuItem.Text = "Calculate";
+            this.calculateToolStripMenuItem.Click += new System.EventHandler(this.calculateToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.backgroundColorToolStripMenuItem.Text = "Background Color";
+            this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorToolStripMenuItem_Click);
+            // 
+            // buttonColorToolStripMenuItem
+            // 
+            this.buttonColorToolStripMenuItem.Name = "buttonColorToolStripMenuItem";
+            this.buttonColorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.buttonColorToolStripMenuItem.Text = "Button Color";
+            this.buttonColorToolStripMenuItem.Click += new System.EventHandler(this.buttonColorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btnColorPicker);
+            this.Controls.Add(this.colorBtn);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
+            this.Controls.Add(this.divideBtn);
+            this.Controls.Add(this.multiplyBtn);
+            this.Controls.Add(this.subtractBtn);
+            this.Controls.Add(this.addBtn);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.helpMenu.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton addBtn;
+        private System.Windows.Forms.RadioButton subtractBtn;
+        private System.Windows.Forms.RadioButton multiplyBtn;
+        private System.Windows.Forms.RadioButton divideBtn;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.ColorDialog colorPicker;
+        private System.Windows.Forms.Button colorBtn;
+        private System.Windows.Forms.Button btnColorPicker;
+        private System.Windows.Forms.ContextMenuStrip helpMenu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItems;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buttonColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/****************************************************************
+    Juan Rodriguez
+    CIST 2341
+    C# Programming 1
+    Lab #4
+simple calculator app
+This is my code....
+*****************************************************************/
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+    // first Check if a text box is empty
+            if (input1.Text == "" || input2.Text == "")
+            {
+                MessageBox.Show("Enter a number!");
+
+            }
+    //by using else-if we avoid an error from stoping our program when textbox is empty
+            else if (addBtn.Checked)
+            {
+               
+                double answer;
+                double inputOne;
+                double inputTwo;
+
+                inputOne = Double.Parse(input1.Text);
+                inputTwo = Double.Parse(input2.Text);
+
+                answer = inputOne + inputTwo;
+
+                answerBox.Text = "" + answer;
+
+            }
+           else if (subtractBtn.Checked)
+            {
+                double answer;
+                double inputOne;
+                double inputTwo;
+
+                inputOne = Double.Parse(input1.Text);
+                inputTwo = Double.Parse(input2.Text);
+
+                answer = inputOne - inputTwo;
+
+                answerBox.Text = "" + answer;
+            }
+            else if (multiplyBtn.Checked)
+            {
+                double answer;
+                double inputOne;
+                double inputTwo;
+
+                inputOne = Double.Parse(input1.Text);
+                inputTwo = Double.Parse(input2.Text);
+
+                answer = inputOne * inputTwo;
+
+                answerBox.Text = "" + answer;
+            }
+            else if (divideBtn.Checked)
+            {
+                double answer;
+                double inputOne;
+                double inputTwo;
+
+                inputOne = Double.Parse(input1.Text);
+                inputTwo = Double.Parse(input2.Text);
+
+                answer = inputOne / inputTwo;
+
+                answerBox.Text = "" + answer;
+            }
+        }
+// Buttons calculate btn ,clear btn, exit btn, background color button, and button color button
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            input1.Text = "";
+            input2.Text = "";
+            answerBox.Text = "";
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            String message = "Are you sure you want to exit?";
+            String caption = "App closing";
+            DialogResult answer = MessageBox.Show(message, caption, MessageBoxButtons.YesNo);
+
+            if (answer == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            if (answer == DialogResult.No)
+            {
+
+            }
+        }
+
+        private void colorBtn_Click(object sender, EventArgs e)
+        {
+            if (colorPicker.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = colorPicker.Color;
+            }
+        }
+
+        private void btnColorPicker_Click(object sender, EventArgs e)
+        {
+            if (colorPicker.ShowDialog() == DialogResult.OK)
+            {
+                calculateBtn.BackColor = colorPicker.Color;
+                clearBtn.BackColor = colorPicker.Color;
+                exitBtn.BackColor = colorPicker.Color;
+
+            }
+        }
+        // MENU ITEMS background-color, button color, calculate, exit, and Help with sub-menu about
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorPicker.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = colorPicker.Color;
+            }
+        }
+
+        private void buttonColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorPicker.ShowDialog() == DialogResult.OK)
+            {
+                calculateBtn.BackColor = colorPicker.Color;
+                clearBtn.BackColor = colorPicker.Color;
+                exitBtn.BackColor = colorPicker.Color;
+
+            }
+        }
+
+        private void calculateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // first Check if a text box is empty
+            if (input1.Text == "" || input2.Text == "")
+            {
+                MessageBox.Show("Enter a number!");
+
+            }
+            //by using else-if we avoid an error from stoping our program when textbox is empty
+            else if (addBtn.Checked)
+            {
+
+                double answer;
+                double inputOne;
+                double inputTwo;
+
+                inputOne = Double.Parse(input1.Text);
+                inputTwo = Double.Parse(input2.Text);
+
+                answer = inputOne + inputTwo;
+
+                answerBox.Text = "" + answer;
+
+            }
+            else if (subtractBtn.Checked)
+            {
+                double answer;
+                double inputOne;
+                double inputTwo;
+
+                inputOne = Double.Parse(input1.Text);
+                inputTwo = Double.Parse(input2.Text);
+
+                answer = inputOne - inputTwo;
+
+                answerBox.Text = "" + answer;
+            }
+            else if (multiplyBtn.Checked)
+            {
+                double answer;
+                double inputOne;
+                double inputTwo;
+
+                inputOne = Double.Parse(input1.Text);
+                inputTwo = Double.Parse(input2.Text);
+
+                answer = inputOne * inputTwo;
+
+                answerBox.Text = "" + answer;
+            }
+            else if (divideBtn.Checked)
+            {
+                double answer;
+                double inputOne;
+                double inputTwo;
+
+                inputOne = Double.Parse(input1.Text);
+                inputTwo = Double.Parse(input2.Text);
+
+                answer = inputOne / inputTwo;
+
+                answerBox.Text = "" + answer;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String message = "Are you sure you want to exit?";
+            String caption = "App closing";
+            DialogResult answer = MessageBox.Show(message, caption, MessageBoxButtons.YesNo);
+
+            if (answer == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            if (answer == DialogResult.No)
+            {
+
+            }
+        }
+// About menu item message box
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String message = "This calculator was created by Juan Rodriguez.";
+            DialogResult  = MessageBox.Show(message);
+        }
+    }
+}
